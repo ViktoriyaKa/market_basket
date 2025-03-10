8d82d5d48fe59d63db86e784fc55751472ab5364
ViktoriyaKa <134738382+ViktoriyaKa@users.noreply.github.com>
2025-03-10 23:26:22 +0300

    Add files via upload

---
+++ chipi/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
+from .models import Product, Review, Category, Shop, Cart, ProdCategory, Order
+
+
+# Register your models here.
+
+
+class CategoryAdmin(MPTTModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'prodcategory', 'shop')
+    search_fields = ['title', 'prodcategory__name']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'count', 'user', 'status')
+    # search_fields = ['title', 'prodcategory__name']
+
+admin.site.register(ProdCategory, CategoryAdmin)
+
+# admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Shop)
+admin.site.register(Review)
+admin.site.register(Cart)
+
+
---
+++ users/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+    is_buyer = models.BooleanField(default=False)
+    is_shop = models.BooleanField(default=False)
+
+
+class Buyer(models.Model):
+    class Age(models.IntegerChoices):
+        MALE = 0, 'Мужской'
+        FEMALE = 1, 'Женский'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, related_name='buyer')
+    first_name = models.CharField(max_length=63, blank=True)
+    middle_name = models.CharField(max_length=63, blank=True)
+    last_name = models.CharField(max_length=63, blank=True)
+    email = models.EmailField(blank=True, unique=True, null=True)
+    phone = models.CharField(blank=True, null=True, unique=True)
+    sex = models.BooleanField(choices=Age.choices, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    correct_address = models.ForeignKey('Address', on_delete=models.CASCADE, related_name='buyer', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Address(models.Model):
+    user = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name='address')
+    first_name = models.CharField(max_length=63)
+    middle_name = models.CharField(max_length=63, blank=True)
+    last_name = models.CharField(max_length=63)
+    email = models.EmailField()
+    phone = models.CharField()
+    country = models.CharField()
+    region = models.CharField()
+    city = models.CharField()
+    index = models.CharField()
+    addr = models.CharField()
+
